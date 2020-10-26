155d619702c74561df875245779576067b7ce910
Karina <karyzenk@gmail.com>
2020-10-27 02:35:08 +0300

    full model book

--- django/src/book/models.py
+++ django/src/book/models.py
@@ -1,56 +1,48 @@
 from django.db import models
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import ForeignKey
-# from sqlalchemy.orm import relationship
-# from sqlalchemy import Column, Integer, String
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# engine = create_engine('sqlite:///book_store.sqlite', echo=False)
-# Session = sessionmaker(bind=engine)
-# session = Session()
-# Base = declarative_base()
-
-# Фото обложки
-# Цена (BYN)
-# Год издания
-# Страниц
-# Переплет
-# Формат
-# ISBN
-# Вес (гр)
-# Возрастные ограничения
-# Количество книг в наличии
-# Активный (доступен для заказа, Да/Нет)
-# Рейтинг (0 - 10)
-# Дата внесения в каталог
-# Дата последнего изменения карточки
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
 
-# Пароль
-# E-mail
-# Имя
-# Фамилия
-# Телефон
-# Группа - недоступно для редактирования. Принадлежность присваивается автоматически
-# Домашний адрес
-# Страна
-# Город
-# Индекс
-# Адрес1
-# Адрес2
-# Дополнительная информация
+class TypeCover(models.TextChoices):
+    HARDCOVER = 'HC', 'Твёрдый переплёт'
+    SOFTCOVER = 'SC', 'Мягкая обложка'
 
-class Book(models.Model):  # Название книги
+
+class AgeRestictions(models.TextChoices):
+    ZERO = '0', '0+'
+    SIX = '6', '6+'
+    TWELVE = '12', '12+'
+    SIXTEEN = '16', '16+'
+    EIGHTEEN = '18', '18+'
+
+
+class Book(models.Model):
     name = models.CharField(
         "Название Книги",
         max_length=100,
-        blank=False,  #Обязательно к заполнению, пустым не может быть
-        null=False  
+        blank=False,
+        null=False
+    )
+    photo = models.ImageField(
+        verbose_name='Фото обложки',
+        upload_to='media/',
+        height_field=100,
+        width_field=100,
+        blank=True,
+        null=True
     )
     author = models.ManyToManyField(
         'handbook.Author',
         verbose_name='Автор/Авторы книги'    
+    )
+    price = models.DecimalField(
+        verbose_name='Цена (BYN)',
+        default=0,
+        max_digits=10,
+        decimal_places=2
+    )
+    year = models.IntegerField(
+        verbose_name='Год издания',
+        validators=[MaxValueValidator(3000), MinValueValidator(0)]
     )
     seria = models.ForeignKey(
         'handbook.BookSeria',
@@ -66,9 +58,70 @@ class Book(models.Model):  # Название книги
         verbose_name='Издательство',
         on_delete=models.PROTECT
     )
+    pages = models.IntegerField(
+        verbose_name='Кол-во страницы',
+        validators=[MinValueValidator(1)]
+    )
+    cover = models.CharField(
+        verbose_name='Переплёт',
+        max_length=2,
+        choices=TypeCover.choices,
+        default=TypeCover.HARDCOVER,
+    )
+    size = models.CharField(
+        verbose_name='Формат',
+        max_length=50,
+        validators=[RegexValidator(
+            regex=r'\d{2,3}x\d{2,3}/\d{2,3}',
+            message='Формат: 120x80/32'
+        )],
+        blank=True,
+        null=True
+    )
+    isbn = models.CharField(
+        verbose_name='ISBN',
+        max_length=17,
+        blank=True,
+        null=True
+    )
+    weight = models.IntegerField(
+        verbose_name='Вес (гр.)',
+        blank=True,
+        null=True
+    )
+    restrictions = models.CharField(
+        verbose_name='Возрастные ограничения',
+        max_length=3,
+        choices=AgeRestictions.choices,
+        default=AgeRestictions.ZERO
+    )
+    book_amount = models.IntegerField(
+        verbose_name='Количество книг в наличии',
+        validators=[MinValueValidator(0)]
+    )
+    created_at = models.DateTimeField(
+        editable=False,
+        auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        editable=False,
+        auto_now=True
+    )
+    available = models.BooleanField(
+        editable=False,
+        default=True
+    )
+    rating = models.FloatField(
+        verbose_name='Рейтинг',
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        default=0
+    )
 
     def __str__(self):
-        return f'{self.name} {self.author.name} {self.seria.name} {self.genre.name} {self.publishing.name}'
+        return (f'{self.name} {self.photo.name} {self.price} {self.author.name} '
+                f'{self.year} {self.seria.name} {self.genre.name} {self.publishing.name} {self.cover}'
+                f'{self.size} {self.isbn} {self.weight} {self.restrictions} {self.book_amount} {self.created_at}'
+                f'{self.modified_at} {self.available} {self.rating}')
 
 
 class Customer(models.Model):  #Профиль покупателя
@@ -83,3 +136,17 @@ class Customer(models.Model):  #Профиль покупателя
 
     def __str__(self):
         return self.login
+
+# Пароль
+# E-mail
+# Имя
+# Фамилия
+# Телефон
+# Группа - недоступно для редактирования. Принадлежность присваивается автоматически
+# Домашний адрес
+# Страна
+# Город
+# Индекс
+# Адрес1
+# Адрес2
+# Дополнительная информация
